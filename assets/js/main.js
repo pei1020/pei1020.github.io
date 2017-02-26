233be27896ab803b7f10f0e6a3a94f81004349bb
Patty Liu <pattyliu@Pattyde-MacBook-Pro.local>
2017-02-26 01:31:22 -0800

    layout fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,18 +55,9 @@
 
 })(jQuery);
 
-
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1200);
+    return false;
 });
-
-
